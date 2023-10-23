1c343ba7a0a0948d03f15d0622419a9aeadda796
MUhAdrian <147995250+MUhAdrian@users.noreply.github.com>
2023-10-23 12:00:37 +0700

    Add files via upload

---
+++ tugas python.py
@@ -0,0 +1,19 @@
+#input nilai variabel
+a=input("variabel a :")
+b=input("variabel b :")
+
+#cetak nilai variabel
+print("variabel a =",a)
+print("variabel b =",b)
+
+#cetak hasil operasi kedua variabel deangan string format
+print("hasil penggabungan {}&{}={}".format(a,b,a+b))
+
+#konversi nilai
+a=int(a)
+b=int(b)
+print("hasil jumlah {}+{}={}".format(a,b,a+b))
+print("hasil jumlah {}/{}={}".format(a,b,a/b))
+
+
+
